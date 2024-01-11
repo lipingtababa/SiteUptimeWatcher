fa88441b9be128212af328f184aaeea7c9e441f2
Ma Chi <hellomarch@gmail.com>
2024-01-11 19:41:29 +0100

    provision roles with terraform

--- infra/dependencies/secrets.tf
+++ infra/dependencies/secrets.tf
@@ -5,3 +5,43 @@ provider "aws" {
 resource "aws_secretsmanager_secret" "default" {
   name = "/watcher/postgre/password"
 }
+
+resource "aws_iam_policy" "secret_reader_policy" {
+  name = "secret_reader_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_secretsmanager_secret.default.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "secret_writer_policy" {
+  name = "secret_writer_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "secretsmanager:PutSecretValue"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_secretsmanager_secret.default.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
